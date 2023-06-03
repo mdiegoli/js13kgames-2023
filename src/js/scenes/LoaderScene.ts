95214bb91b50e54fa93fdaaaa68992827e67fa65
Michele <michelediegoli123@gmail.com>
2023-06-04 00:24:50 +0200

    added pacworm sprites

--- src/js/scenes/LoaderScene.ts
+++ src/js/scenes/LoaderScene.ts
@@ -2,7 +2,7 @@ export default class LoaderScene extends Phaser.Scene {
   public preload() {
     this.load.image("tiles", "./assets/images/tiles.png");
     this.load.audio("jump", "./assets/audio/jump.mp3");
-    this.load.spritesheet("player", "./assets/images/player.png", { frameWidth: 16, frameHeight: 32 });
+    this.load.spritesheet("player", "./assets/images/hero.png", { frameWidth: 16, frameHeight: 16 });
   }
 
   public create() {
